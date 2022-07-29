e511b818a470433930b815df544f197db7b2b781
Tatiana Lyapina <antipts@gmail.com>
2022-07-29 20:35:01 +0300

    Add JUnit Vintage + test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,11 @@
 package ru.netology.service;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 
 import static org.junit.Assert.*;
 
@@ -20,7 +23,6 @@ public class CashbackHackServiceTest {
         service = null;
     }
 
-
     @Test
     public void shouldRemain50() {
         int amount = 950;
@@ -29,7 +31,6 @@ public class CashbackHackServiceTest {
 
         assertEquals(expected, actual);
     }
-
 
     @Test
     public void shouldRemain0() {
@@ -40,7 +41,6 @@ public class CashbackHackServiceTest {
         assertEquals(expected, actual);
     }
 
-
     @Test
     public void shouldRemain300() {
         int amount = 1700;
@@ -48,6 +48,33 @@ public class CashbackHackServiceTest {
         int expected = 300;
 
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemain112JUnit5() {
+        int amount = 888;
+        int actual = service.remain(amount);
+        int expected = 112;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemain0JUnit5() {
+        int amount = 2000;
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemain150JUnit5() {
+        int amount = 2850;
+        int actual = service.remain(amount);
+        int expected = 150;
+
+        Assertions.assertEquals(expected, actual);
     }
 
 }
